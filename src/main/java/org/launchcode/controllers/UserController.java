d91e2f391dbfa88452e42199c25277a1cc9b22c0
Cypress <kad2321@yahoo.com>
2024-10-17 20:00:44 -0400

    Studio Start

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,10 +1,28 @@
 package org.launchcode.controllers;
 
 
+import ch.qos.logback.core.model.Model;
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("user")
+@ResponseBody
 public class UserController {
+
+    @GetMapping("/user/add")
+    public String displayAddUserForm() {
+        return "resources/templates/user/add.html";
+    }
+
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        return ":P";
+    }
+
+
+
 }
